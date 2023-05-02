5ea2b0a935de5799eb4b58aad00512515c9ac1a3
kmanthrige <k.manthrige@gmail.com>
2023-05-02 11:27:28 -0500

    completed assignment

---
+++ assignment.js
@@ -0,0 +1,72 @@
+console.log('Week 5 Assignment 1')
+
+console.log('***** Object Practice *****')
+
+// Add the required code to complete the tasks below
+
+/* 1. Create an object literal to represent you!
+  - Give the object a 'firstName & (separate) 'lastName' property
+  - Give it a boolean 'hasSiblings' property
+  - Give it  a numeric 'shoeCount' property
+  - Also give it an array 'favThreeFoods'
+*/
+const me = {
+    firstName: 'Kushani',
+    lastName: 'Manthrige',
+    hasSiblings: true,
+    shoeCount: 10,
+    favThreeFoods: ['pizza', 'chocolate', 'sushi'],
+};
+console.log('A little about me:', me);
+
+/* 2. Accessing object properties.
+  - Create a variable called fullName 
+  - Use the firstName & lastName properties of the object you 
+    created above to set its value. 
+  - Console.log fullName
+*/
+
+let fullName = me.firstName + ' ' + me.lastName
+
+console.log(fullName)
+/* 3. Nested arrays
+  - Console.log your first favorite food
+  - Console.log your last favorite food 
+*/
+
+console.log('first fav food:', me.favThreeFoods[0])
+console.log('last fav food:', me.favThreeFoods[me.favThreeFoods.length - 1])
+
+
+/* 4. Change a property of an existing object.
+  - Console.log your current shoe count.
+  - Guess what?! You just got a new pair of shoes!!!
+  - Set your object's shoeCount to be what it was plus 1
+  - Console.log your updated shoe count. 
+*/
+console.log('number of shoes:', me.shoeCount)
+
+me.shoeCount = 11
+
+console.log('new number of shoes after shopping:', me.shoeCount)
+
+let newShoesBought = 8
+
+if (newShoesBought > 0) {
+    console.log('if statement: new number of shoes:', me.shoeCount + newShoesBought)
+}
+else {
+    console.log('if statement: did not go shopping:', me.shoeCount)
+}
+
+
+
+/* 5. Add a new property to an existing object.
+  - Add a 'favoriteColor' to your existing object
+  - Do this the same way you updated the variable above
+  - Cool huh? It works, even though it wasn't there before
+  - Console.log your object
+*/
+me.favouriteColor = 'green'
+
+console.log ('me', me)
